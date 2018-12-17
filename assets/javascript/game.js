7d2b8c1c915ae1f9599a997eb92aa0ceca7872ff
Hemal Patel <44946178+origeagles@users.noreply.github.com>
2018-12-17 16:40:42 -0500

    Create game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//variables
+var wordLength
+var wins = 0
+var losses = 0
+var guesses = 7
+var userGuess
+
+// Table array for word guesses//
+var keyWords = ["stark", "banner", "thor", "rodgers", "groot", "parker", "vision", "fury", "avengers", "marvel", "barton", "romanoff", "barnes", "loki", "quill", "gamora", "rocket", "drax", "wanda", "ultron", "thanos", "lang", "hope", "strange"];
+
