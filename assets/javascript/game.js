5fc6dbd01eb93ab6be2340c8959f6e26d34815a9
Hemal Patel <44946178+origeagles@users.noreply.github.com>
2018-12-17 17:18:24 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,11 +8,11 @@ var userGuess
 
 // Table array for word guesses//
 var keyWords = ["stark", "banner", "thor", "rodgers", "groot", "parker", "vision", "fury", "avengers", "marvel", "barton", "romanoff", "barnes", "loki", "quill", "gamora", "rocket", "drax", "wanda", "ultron", "thanos", "lang", "hope", "strange"];
-console.log(keyWords)
+console.log(keyWords);
 
 // Generate random word to guess//
 // wordChosen = [Math.floor(Math.random(keyWords) * keyWords.length)];
 const wordChosen = keyWords[Math.floor(Math.random() * keyWords.length)];
 
-console.log(wordChosen)
+console.log(wordChosen);
 
